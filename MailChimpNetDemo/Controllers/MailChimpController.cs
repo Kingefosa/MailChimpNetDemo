26f8dbf1453233cbd70357a7b9333b12cbbec782
Doug Vanderweide <dvanderweide@mainetoday.com>
2016-07-22 09:30:30 -0400

    Add project files.

---
+++ MailChimpNetDemo/Controllers/MailChimpController.cs
@@ -0,0 +1,39 @@
+﻿using MailChimp.Net;
+using MailChimp.Net.Core;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MailChimpNetDemo.Controllers
+{
+    public class MailChimpController : Controller
+    {
+        private static readonly MailChimpManager Manager = new MailChimpManager();
+
+        public async Task<ActionResult> SentCampaigns()
+        {
+            var options = new CampaignRequest
+            {
+                ListId = "rrnef25ixy",
+                Status = CampaignStatus.Sent,
+                SortOrder = CampaignSortOrder.DESC,
+                Limit = 10
+            };
+
+            try
+            {
+                var model = await Manager.Campaigns.GetAllAsync(options);
+                return View(model);
+            }
+            catch (MailChimpException mce)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadGateway, mce.Message);
+            }
+            catch(Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.ServiceUnavailable, ex.Message);
+            }
+        }
+    }
+}
---
+++ MailChimpNetDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MailChimpNetDemo.MvcApplication" Language="C#" %>
